39fe5ffa25083063ac6dc4a37d8090e9688d0250
TomMulvaney <mulvaney.tom@gmail.com>
2016-10-10 18:01:55 +0100

    Created Vector4

---
+++ Assets/Behaviours/Vector4.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vector4 : MonoBehaviour {
+
+    
+
+    public float w = 0f;
+    public float x = 0f;
+    public float y = 0f;
+    public float z = 0f;
+}
