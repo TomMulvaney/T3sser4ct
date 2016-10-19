d169b787eb12234fa92aadd6d827245bc95fef8a
TomMulvaney <mulvaney.tom@gmail.com>
2016-10-19 20:50:25 +0100

    Created FPSMouse and GalleryWalker scripts. Added them to player in Nets

---
+++ Assets/Behaviours/GalleryWalker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GalleryWalker : MonoBehaviour {
+    [SerializeField]
+    private Transform root;
+
+	// Use this for initialization
+	void Start () {
+        if (root == null) {
+            root = transform;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
